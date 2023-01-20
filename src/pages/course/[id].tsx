b0dbe48a30fc24fce88c3e21bfcdb9f265bfd753
Edson Mallet <edsonmmr@gmail.com>
2023-01-20 03:00:49 -0300

    feat: add landing page course

--- src/pages/course/[id].tsx
+++ src/pages/course/[id].tsx
@@ -1,34 +1,159 @@
+import {
+  Box,
+  Button,
+  Flex,
+  Heading,
+  HStack,
+  Text,
+  useColorModeValue,
+} from "@chakra-ui/react";
+import { Flag } from "phosphor-react";
 import { Header } from "../../components/Header";
 import { Player } from "../../components/Player";
 import { PrivatePageTemplate } from "../../components/PrivatePageTemplate";
 import { SideBar } from "../../components/SideBar";
+import { TitlePage } from "../../components/TitlePage";
 import { VideoInformation } from "../../components/VideoInformation";
 import api from "../../services/api";
 
-export default function CoursePage() {
-  return (
-    <PrivatePageTemplate
-      header={<Header />}
-      main={
-        <>
-          <Player />
-          <VideoInformation />
-        </>
-      }
-      sidebar={<SideBar />}
-    />
+export default function CourseDetailPage() {
+  const Details = () => (
+    <Flex
+      w="full"
+      my={10}
+      alignItems={"center"}
+      justifyContent="flex-start"
+      direction={"column"}
+    >
+      <Flex
+        gap={10}
+        wrap="wrap"
+        maxW={"full"}
+        justifyContent="center"
+        alignItems="center"
+      >
+        <Flex gap={8} w="50vw">
+          <Box w="70%">Image</Box>
+          <Flex direction={"column"} gap={4}>
+            <Text
+              color={"green.500"}
+              textTransform={"uppercase"}
+              fontWeight={400}
+              fontSize={"2xl"}
+              letterSpacing={1.1}
+            >
+              TECH
+            </Text>
+            <Heading
+              color={useColorModeValue("gray.700", "white")}
+              fontSize={"2xl"}
+              fontFamily={"body"}
+            >
+              Boost your conversion rate
+            </Heading>
+            <Text color={"gray.500"}>
+              Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+              nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
+              erat, sed diam voluptua. At vero eos et accusam et justo duo
+              dolores et ea rebum.
+            </Text>
+            <Button w="full" colorScheme={"green"}>
+              Continue Aprendendto
+            </Button>
+          </Flex>
+        </Flex>
+
+        <Flex gap={8} w="50vw" py={50}>
+          <Flex w={"25%"} h={100} bgColor="#B3C52D">
+            Image
+          </Flex>
+          <Flex w={"25%"} h={100} bgColor="#B3C52D70">
+            Image
+          </Flex>
+          <Flex w={"25%"} h={100} bgColor="#B3C52D">
+            Image
+          </Flex>
+          <Flex w={"25%"} h={100} bgColor="#B3C52D70">
+            Image
+          </Flex>
+        </Flex>
+
+        <Flex gap={8} w="full" bgColor="#B3C52D70" justifyContent="center">
+          <Flex w={"50vw"} py={50} justifyContent="center">
+            <Heading> Com esse curso você estará apto a </Heading>
+          </Flex>
+        </Flex>
+
+        <Flex gap={8} w="full" justifyContent="center">
+          <Flex w={"50vw"} py={50} justifyContent="space-between">
+            <Flex flex={1} justifyContent="center">
+              <Flex direction={"column"}>
+                <Heading>
+                  O que você vai{" "}
+                  <span style={{ color: "#B3C52D" }}>aprender</span>
+                </Heading>
+                <Text mt={5}>
+                  Os conteúdos ofertados aqui são embassados nas experiências
+                  dos instrutores no mercado.
+                </Text>
+              </Flex>
+
+              <Flex gap={4} wrap="wrap">
+                <Flex alignItems={"stretch"} w="40%" gap={2}>
+                  <Flex>
+                    <Flag fontSize={44} weight="fill" />
+                  </Flex>
+                  <Flex direction={"column"}>
+                    <Heading fontSize={20}>Metodologia</Heading>
+                    <Text>
+                      Você vai conferir aulas em vídeo com direcionamento claro
+                      e prático. Também vale a pena consultar e ler com atenção
+                      o material de apoio para fixar os aprendizados.
+                    </Text>
+                  </Flex>
+                </Flex>
+
+                <Flex alignItems={"stretch"} w="40%" gap={2}>
+                  <Flex>
+                    <Flag fontSize={44} weight="fill" />
+                  </Flex>
+                  <Flex direction={"column"}>
+                    <Heading fontSize={20}>Metodologia</Heading>
+                    <Text>
+                      Você vai conferir aulas em vídeo com direcionamento claro
+                      e prático. Também vale a pena consultar e ler com atenção
+                      o material de apoio para fixar os aprendizados.
+                    </Text>
+                  </Flex>
+                </Flex>
+
+                <Flex alignItems={"stretch"} w="40%" gap={2}>
+                  <Flex>
+                    <Flag fontSize={44} weight="fill" />
+                  </Flex>
+                  <Flex direction={"column"}>
+                    <Heading fontSize={20}>Metodologia</Heading>
+                    <Text>
+                      Você vai conferir aulas em vídeo com direcionamento claro
+                      e prático. Também vale a pena consultar e ler com atenção
+                      o material de apoio para fixar os aprendizados.
+                    </Text>
+                  </Flex>
+                </Flex>
+              </Flex>
+            </Flex>
+          </Flex>
+        </Flex>
+
+        <Flex gap={8} w="full" bgColor="#B3C52D70" justifyContent="center">
+          <Flex w={"50vw"} py={50} justifyContent="center">
+            <Heading>
+              Pra <span style={{ color: "#B3C52D" }}>quem</span> é
+            </Heading>
+          </Flex>
+        </Flex>
+      </Flex>
+    </Flex>
   );
-}
-
-export async function getStaticProps() {
-  const res = await api.get("/courses");
-
-  console.log(res.data);
-
-  return {
-    props: {
-      revalidate: 60,
-      courses: res.data,
-    },
-  };
+  return <PrivatePageTemplate header={<Header />} main={<Details />} />;
 }
--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -1,15 +1,28 @@
+import { Flex } from "@chakra-ui/react";
 import { Header } from "../components/Header";
 import { PrivatePageTemplate } from "../components/PrivatePageTemplate";
+import { TitlePage } from "../components/TitlePage";
 
 export default function ProfilePage() {
-  return (
-    <PrivatePageTemplate
-      header={<Header />}
-      main={
-        <>
-          <p>profile</p>
-        </>
-      }
-    />
+  const Profile = () => (
+    <Flex
+      my={10}
+      alignItems={"center"}
+      justifyContent="flex-start"
+      direction={"column"}
+    >
+      <TitlePage title="Profile" />
+
+      <Flex
+        gap={10}
+        wrap="wrap"
+        maxW={"80vw"}
+        justifyContent="center"
+        alignItems="center"
+      >
+        <p>profile</p>
+      </Flex>
+    </Flex>
   );
+  return <PrivatePageTemplate header={<Header />} main={<Profile />} />;
 }
