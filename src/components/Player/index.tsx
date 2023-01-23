0fdd5b6e9d9b8e748961153e5913ae2c6ed079c8
Edson Mallet <edsonmmr@gmail.com>
2023-01-23 09:55:45 -0300

    fix: add module content lesson

--- src/components/Player/index.tsx
+++ src/components/Player/index.tsx
@@ -4,15 +4,19 @@ import { useLessonStore } from "../../stores";
 export const Player = () => {
   const { currentLesson } = useLessonStore();
   return (
-    <HStack w="full" justify="center" px="8" bg="white">
-      <AspectRatio maxW="container.lg" w="full" ratio={16 / 9}>
-        <iframe
-          width="560"
-          src={`${currentLesson?.attributes?.urlVideo}?autoplay=0&showinfo=0&controls=1&rel=0&modestbranding=1&playsinline=0`}
-          title="YouTube video player"
-          allowFullScreen
-        />
-      </AspectRatio>
-    </HStack>
+    <>
+      {currentLesson?.attributes?.urlVideo && (
+        <HStack w="full" justify="center" px="8" bg="white">
+          <AspectRatio maxW="container.lg" w="full" ratio={16 / 9}>
+            <iframe
+              width="560"
+              src={`${currentLesson?.attributes?.urlVideo}?autoplay=0&showinfo=0&controls=1&rel=0&modestbranding=1&playsinline=0`}
+              title="YouTube video player"
+              allowFullScreen
+            />
+          </AspectRatio>
+        </HStack>
+      )}
+    </>
   );
 };
