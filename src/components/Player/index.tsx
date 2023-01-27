b402d8e33d32565d6111811a9563837f548167cf
Edson Mallet <edsonmmr@gmail.com>
2023-01-27 15:50:16 -0300

    fix: update video size

--- src/components/Player/index.tsx
+++ src/components/Player/index.tsx
@@ -7,7 +7,7 @@ export const Player = () => {
     <>
       <HStack w="full" bg="black" justify="center">
         {currentLesson?.attributes?.urlVideo ? (
-          <AspectRatio maxW="980px" w="full" ratio={16 / 9}>
+          <AspectRatio maxW="800px" w="full" ratio={16 / 9}>
             <iframe
               width="100%"
               src={`${currentLesson?.attributes?.urlVideo}?autoplay=0&showinfo=0&controls=1&rel=0&modestbranding=1&playsinline=0`}
